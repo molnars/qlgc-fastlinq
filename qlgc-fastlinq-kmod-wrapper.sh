2f4c7882ed2da033d7951be7d2515e5c4b566924
molnars <szabolcs.molnar@gmail.com>
2022-02-07 14:26:01 +0400

    Update qlgc-fastlinq-kmod-wrapper.sh

--- qlgc-fastlinq-kmod-wrapper.sh
+++ qlgc-fastlinq-kmod-wrapper.sh
@@ -3,4 +3,4 @@
 # This is a thin wrapper that maps userspace tools (executables)
 # to a container where they can be run.
 
-kmods-via-containers wrapper qlgc-fastline-kmod $(uname -r) $(basename $0) $@
+kmods-via-containers wrapper qlgc-fastlinq-kmod $(uname -r) $(basename $0) $@
