1202124ffdf3c713f4fcfa734700a4f9eea8a7a1
molnars <szabolcs.molnar@gmail.com>
2022-07-25 22:04:12 +0400

    Create blacklist_kernelargs.sh

---
+++ blacklist_kernelargs.sh
@@ -0,0 +1,1 @@
+rpm-ostree kargs --append rd.driver.blacklist=qedf --append rd.driver.blacklist=qed --append rd.driver.blacklist=qedr --append rd.driver.blacklist=qedi --append rd.driver.blacklist=qede
